d7865342274b328047d18b6dba70dda68398edd8
Julian Hüppauff <julian@hueppauff.com>
2017-11-04 22:57:10 +0100

    removed usings

--- Dynamics365 Customizing Downloader/Cryptography.cs
+++ Dynamics365 Customizing Downloader/Cryptography.cs
@@ -11,12 +11,9 @@
 namespace Dynamics365CustomizingDownloader
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Security.Cryptography;
     using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// Cryptography Extention using Rijndael
