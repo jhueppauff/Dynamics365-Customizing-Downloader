e0b8080c6324646a71e163d0c2643b1f454d5ffc
Julian Hüpppauff <julian@hueppauff.info>
2017-11-22 18:57:59 +0100

    Fix UnitTest

--- Dynamics365 Customizing Downloader/Cryptography.cs
+++ Dynamics365 Customizing Downloader/Cryptography.cs
@@ -32,7 +32,6 @@ namespace Dynamics365CustomizingDownloader
         /// DecryptStringAES().  The sharedSecret parameters must match.
         /// </summary>
         /// <param name="plainText">The text to encrypt.</param>
-        /// <param name="sharedSecret">A password used to generate a key for encryption.</param>
         /// <returns>Returns a encrypted string</returns>
         public static string EncryptStringAES(string plainText)
         {
@@ -56,6 +55,75 @@ namespace Dynamics365CustomizingDownloader
             {
                 // generate the key from the shared secret and the salt
                 Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MainWindow.EncryptionKey, salt);
+
+                // Create a RijndaelManaged object
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    // prepend the IV
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            // Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                {
+                    aesAlg.Clear();
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return outStr;
+        }
+
+        /// <summary>
+        /// Encrypt the given string using AES.  The string can be decrypted using 
+        /// DecryptStringAES().  The sharedSecret parameters must match.
+        /// </summary>
+        /// <param name="plainText">The text to encrypt.</param>
+        /// <param name="sharedSecret">A password used to generate a key for encryption.</param>
+        /// <returns>Returns a encrypted string</returns>
+        public static string EncryptStringAES(string plainText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(plainText))
+            {
+                throw new ArgumentNullException("plainText");
+            }
+
+            if (string.IsNullOrEmpty(sharedSecret))
+            {
+                throw new ArgumentNullException("sharedSecret");
+            }
+
+            // Encrypted string to return
+            string outStr = null;
+
+            // RijndaelManaged object used to encrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, salt);
 
                 // Create a RijndaelManaged object
                 aesAlg = new RijndaelManaged();
@@ -126,6 +194,76 @@ namespace Dynamics365CustomizingDownloader
             {
                 // generate the key from the shared secret and the salt
                 Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MainWindow.EncryptionKey, salt);
+
+                // Create the streams used for decryption.                
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+                    // Create a RijndaelManaged object
+                    // with the specified key and IV.
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+                    // Get the initialization vector from the encrypted stream
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plainText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                {
+                    aesAlg.Clear();
+                }
+            }
+
+            return plainText;
+        }
+
+        /// <summary>
+        /// Decrypt the given string.  Assumes the string was encrypted using 
+        /// EncryptStringAES(), using an identical sharedSecret.
+        /// </summary>
+        /// <param name="cipherText">The text to decrypt.</param>
+        /// <param name="sharedSecret">Shared Secret</param>
+        /// <returns>Returns a decrypted string</returns>
+        public static string DecryptStringAES(string cipherText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+            {
+                throw new ArgumentNullException("cipherText");
+            }
+
+            if (string.IsNullOrEmpty(sharedSecret))
+            {
+                throw new ArgumentNullException("sharedSecret");
+            }
+
+            // Declare the RijndaelManaged object
+            // used to decrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plainText = null;
+
+            try
+            {
+                // generate the key from the shared secret and the salt
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, salt);
 
                 // Create the streams used for decryption.                
                 byte[] bytes = Convert.FromBase64String(cipherText);
