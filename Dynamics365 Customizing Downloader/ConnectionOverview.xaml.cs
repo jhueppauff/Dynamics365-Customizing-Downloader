bfefa4a428a734d15668a9b40226ad00eaa19b03
Julian Hüpppauff <julian@hueppauff.info>
2017-11-24 09:53:44 +0100

    Added Connection Overview Window

---
+++ Dynamics365 Customizing Downloader/ConnectionOverview.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dynamics365CustomizingDownloader
+{
+    /// <summary>
+    /// Interaction logic for ConnectionOverview.xaml
+    /// </summary>
+    public partial class ConnectionOverview : Window
+    {
+        public ConnectionOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
