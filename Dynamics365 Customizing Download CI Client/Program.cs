69f20b4d86146822bba6c518f7d7ffea1ce33a5a
Julian Hüpppauff <julian@hueppauff.info>
2017-12-16 23:44:55 +0100

    Added Dispose

--- Dynamics365 Customizing Download CI Client/Program.cs
+++ Dynamics365 Customizing Download CI Client/Program.cs
@@ -16,7 +16,7 @@ namespace Dynamics365CustomizingDownloader.CIClient
     using System.Linq;
     using Microsoft.Xrm.Tooling.Connector;
 
-    class Program
+    class Program : IDisposable
     {
         /// <summary>
         /// Runtime Parameters
@@ -42,6 +42,8 @@ namespace Dynamics365CustomizingDownloader.CIClient
                     Console.Write(log);
                 }
             }
+
+            toolingConnector.Dispose();
         }
 
         private static void GetParameter(string[] args)
@@ -90,5 +92,40 @@ namespace Dynamics365CustomizingDownloader.CIClient
                 throw;
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    parameters = null;
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Program() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
