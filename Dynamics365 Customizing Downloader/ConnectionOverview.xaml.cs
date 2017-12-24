be1dfee785740eae76f7dc92934fdee8a623921a
Julian Hüpppauff <julian@hueppauff.info>
2017-12-24 13:25:13 +0100

    Added Check if a Connection was selected

--- Dynamics365 Customizing Downloader/ConnectionOverview.xaml.cs
+++ Dynamics365 Customizing Downloader/ConnectionOverview.xaml.cs
@@ -155,6 +155,12 @@ namespace Dynamics365CustomizingDownloader
         /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void Btn_DeleteRepository_Click(object sender, RoutedEventArgs e)
         {
+            if (Cbx_CRMConnections.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a CRM Connection first", "Missing Repository", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             if (MessageBox.Show("Are you sure to delete the Repository?", "Are you sure?", MessageBoxButton.YesNo, MessageBoxImage.Stop, MessageBoxResult.No) == MessageBoxResult.Yes)
             {
                 if (Directory.Exists(this.crmConnection.LocalPath))
