f1e4e17939ab49f2f4bebe0be8749cd8549ae945
Julian Hüpppauff <julian@hueppauff.info>
2017-12-25 08:41:48 +0100

    Added Basic Controls

--- Dynamics365 Customizing Downloader/Pages/SolutionDownloader.xaml.cs
+++ Dynamics365 Customizing Downloader/Pages/SolutionDownloader.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Dynamics365CustomizingDownloader.Pages
+﻿namespace Dynamics365CustomizingDownloader.Pages
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for SolutionDownloader.xaml
     /// </summary>
     public partial class SolutionDownloader : Page
     {
+        /// <summary>
+        /// Log4Net Logger
+        /// </summary>
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public SolutionDownloader()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                Diagnostics.ErrorReport errorReport = new Diagnostics.ErrorReport(ex);
+                errorReport.Show();
+            }
+        }
+
+        private void btn_download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Reload_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
