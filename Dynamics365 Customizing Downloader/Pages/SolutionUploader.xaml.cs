a79e61afacb69c1d610c5717a23b027aad0a44a3
Julian Hüpppauff <julian@hueppauff.com>
2018-01-23 20:12:32 +0100

    Added UI for Solution Upload

--- Dynamics365 Customizing Downloader/Pages/SolutionUploader.xaml.cs
+++ Dynamics365 Customizing Downloader/Pages/SolutionUploader.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//-----------------------------------------------------------------------
+// <copyright file="SolutionUploader.xaml.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2017 Jhueppauff
+// MIT  
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Dynamics365CustomizingDownloader.Pages
 {
+    using System.Windows.Controls;
+
     /// <summary>
-    /// Interaction logic for SolutionUploader.xaml
+    /// Interaction logic for SolutionUploader
     /// </summary>
     public partial class SolutionUploader : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SolutionUploader"/> class.
+        /// </summary>
         public SolutionUploader()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void Cbx_Connection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
