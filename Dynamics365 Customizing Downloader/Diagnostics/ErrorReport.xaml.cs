063282520a068b81edd788c7dfb3d039fc51456c
Julian Hüpppauff <julian@hueppauff.com>
2018-02-01 23:12:40 +0100

    added Message box to show the error if it occures in the error dialog

--- Dynamics365 Customizing Downloader/Diagnostics/ErrorReport.xaml.cs
+++ Dynamics365 Customizing Downloader/Diagnostics/ErrorReport.xaml.cs
@@ -47,6 +47,7 @@ namespace Dynamics365CustomizingDownloader.Diagnostics
             catch (Exception exc)
             {
                 Log.Error(exc.Message, exc);
+                MessageBox.Show("There was an error showing the error Dialog", "Ups, you broke it", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
