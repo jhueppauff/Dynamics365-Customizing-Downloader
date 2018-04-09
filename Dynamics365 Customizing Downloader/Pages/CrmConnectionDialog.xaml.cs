3227e6db86a57c84c8379ad5bffc3bd42b4794c9
Julian Hüpppauff <julian@hueppauff.com>
2018-04-09 07:59:50 +0200

    added Copyright header, moved usings

--- Dynamics365 Customizing Downloader/Pages/CrmConnectionDialog.xaml.cs
+++ Dynamics365 Customizing Downloader/Pages/CrmConnectionDialog.xaml.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿//-----------------------------------------------------------------------
+// <copyright file="DownloadSolution.xaml.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2018 Jhueppauff
+// Mozilla Public License Version 2.0 
+// For licence details visit https://github.com/jhueppauff/Dynamics365-Customizing-Downloader/blob/master/LICENSE
+//-----------------------------------------------------------------------
 
 namespace Dynamics365CustomizingDownloader.Pages
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for CrmConnectionDialog.xaml
     /// </summary>
