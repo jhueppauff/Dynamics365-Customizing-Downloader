7ef36bf6768ac2b093061361c094b68d9a7962dd
Julian Hüpppauff <julian@hueppauff.com>
2018-04-09 19:02:00 +0200

    initial add pluginbase

---
+++ Dynamics365 Customizing Downloader Core/Plugin/PluginBase.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2018 Jhueppauff
+// Mozilla Public License Version 2.0 
+// For licence details visit https://github.com/jhueppauff/Dynamics365-Customizing-Downloader/blob/master/LICENSE
+//-----------------------------------------------------------------------
+
+namespace Dynamics365CustomizingDownloader.Core.Plugin
+{
+    public class PluginBase
+    {
+
+    }
+}
