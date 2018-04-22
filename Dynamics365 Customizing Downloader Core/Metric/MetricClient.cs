5af84ec4eb17386fa0c23928cfe2c30144a01345
Julian Hüpppauff <julian@hueppauff.com>
2018-04-22 21:13:16 +0200

    added metric client

---
+++ Dynamics365 Customizing Downloader Core/Metric/MetricClient.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MetricClient.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2018 Jhueppauff
+// Mozilla Public License Version 2.0 
+// For licence details visit https://github.com/jhueppauff/Dynamics365-Customizing-Downloader/blob/master/LICENSE
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Dynamics365CustomizingDownloader.Core.Metric
+{
+    public class MetricClient
+    {
+    }
+}
