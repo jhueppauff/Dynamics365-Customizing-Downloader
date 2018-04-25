ca47c56eaed1db91b1e4e7d8032ef9e664484f8c
Julian Hüpppauff <julian@hueppauff.com>
2018-04-25 19:54:05 +0200

    added header, constructor

--- Dynamics365 Customizing Downloader/AppMetricHelper.cs
+++ Dynamics365 Customizing Downloader/AppMetricHelper.cs
@@ -1,6 +1,23 @@
-﻿namespace Dynamics365CustomizingDownloader
+﻿//-----------------------------------------------------------------------
+// <copyright file="AppMetricHelper.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2018 Jhueppauff
+// Mozilla Public License Version 2.0 
+// For licence details visit https://github.com/jhueppauff/Dynamics365-Customizing-Downloader/blob/master/LICENSE
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Dynamics365CustomizingDownloader
 {
     public class AppMetricHelper
     {
+        /// <summary>
+        /// Identifier of the Application
+        /// </summary>
+        private readonly string applicationId;
+
+        public AppMetricHelper(string applicationId)
+        {
+            this.applicationId = applicationId;
+        }
     }
 }
