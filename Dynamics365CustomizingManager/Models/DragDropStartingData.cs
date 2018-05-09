d4326df070eb43de1fb0d018c18e6fa7f9a2cf1d
Julian Hüpppauff <julian@hueppauff.com>
2018-05-09 10:25:59 +0200

    initial UWP

---
+++ Dynamics365CustomizingManager/Models/DragDropStartingData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Dynamics365CustomizingManager.Models
+{
+    public class DragDropStartingData
+    {
+        public DataPackage Data { get; set; }
+
+        public IList<object> Items { get; set; }
+    }
+}
---
+++ Dynamics365CustomizingManager/Services/FirstRunDisplayService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Dynamics365CustomizingManager.Views;
+
+using Microsoft.Toolkit.Uwp.Helpers;
+
+namespace Dynamics365CustomizingManager.Services
+{
+    public static class FirstRunDisplayService
+    {
+        private static bool shown = false;
+
+        internal static async Task ShowIfAppropriateAsync()
+        {
+            if (SystemInformation.IsFirstRun && !shown)
+            {
+                shown = true;
+                var dialog = new FirstRunDialog();
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
