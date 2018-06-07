8fa73699f8e9fe6bd1f353d9467af9e50bcc7190
Julian Hüpppauff <julian@hueppauff.com>
2018-06-07 22:50:57 +0200

    added option class

---
+++ Dynamics365 Customizing Download CI Client/Options.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Options.cs" company="https://github.com/jhueppauff/Dynamics365-Customizing-Downloader">
+// Copyright 2018 Jhueppauff
+// Mozilla Public License Version 2.0 
+// For licence details visit https://github.com/jhueppauff/Dynamics365-Customizing-Downloader/blob/master/LICENSE
+// </copyright>
+//-----------------------------------------------------------------------
+
+
+
+namespace Dynamics365CustomizingDownload.CIClient
+{
+    using CommandLine;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Options
+    {
+        [Option('c', "Connection Name", Required = true, HelpText = "Name of the Connection")]
+        public string ConnectionName { get; set; }
+
+    }
+}
