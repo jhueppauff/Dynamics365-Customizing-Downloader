f3b4eff942c3be93bcb780a2ce19f8053c84793b
Julian Hüpppauff <julian@hueppauff.com>
2018-06-09 11:45:56 +0200

    added options

--- Dynamics365 Customizing Download CI Client/Options.cs
+++ Dynamics365 Customizing Download CI Client/Options.cs
@@ -24,5 +24,11 @@ namespace Dynamics365CustomizingDownload.CIClient
 
         [Option('s', "Solution Name", Required = true, HelpText = "Name of the Solution to Download")]
         public string SolutionName { get; set; }
+
+        [Option('e', "Extract String", Required = false, HelpText = "Specify if you want to extract strings into ressource Files" )]
+        public bool ExtractStrings { get; set; }
+
+        [Option('a', "Action", Required = true, HelpText = "Specify the action to run, Download, Upload, DownloadAndExtract")]
+        public string Action { get; set; }
     }
 }
