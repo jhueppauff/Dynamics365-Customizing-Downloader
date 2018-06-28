796d0e1d9857bad084827def043e88d0f36ca8ef
Julian Hüpppauff <julian@hueppauff.info>
2018-06-28 12:09:47 +0200

    updated assembly infos, assets

--- Dynamics365CustomizingManager/Dynamics365CustomizingManager/Properties/AssemblyInfo.cs
+++ Dynamics365CustomizingManager/Dynamics365CustomizingManager/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dynamics365CustomizingManager")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Julian Hüppauff 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: ComVisible(false)]
