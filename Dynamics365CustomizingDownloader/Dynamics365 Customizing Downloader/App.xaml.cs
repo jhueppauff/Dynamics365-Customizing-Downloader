80c0b0b15db5ef675205bc8f2fe78690cb04a70c
Julian Hüpppauff <julian@hueppauff.com>
2018-10-01 23:08:46 +0200

    removed fault domain

--- Dynamics365CustomizingDownloader/Dynamics365 Customizing Downloader/App.xaml.cs
+++ Dynamics365CustomizingDownloader/Dynamics365 Customizing Downloader/App.xaml.cs
@@ -8,7 +8,6 @@
 
 namespace Dynamics365CustomizingDownloader
 {
-    using System;
     using System.Windows;
 
     /// <summary>
@@ -16,45 +15,5 @@ namespace Dynamics365CustomizingDownloader
     /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Overwrite for the Application Startup
-        /// </summary>
-        /// <param name="e">The <see cref="StartupEventArgs"/> instance containing the event data.</param>
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            try
-            {
-                // We use this to get access to unhandled exceptions so we can 
-                // report app crashes to the Telemetry client
-                var currentDomain = AppDomain.CurrentDomain;
-
-                currentDomain.UnhandledException += this.CurrentDomain_UnhandledException;
-                currentDomain.ProcessExit += this.CurrentDomain_ProcessExit;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-        }
-
-        /// <summary>
-        /// Handles the close of the Application
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="UnhandledExceptionEventArgs"/> instance containing the event data.</param>
-        private void CurrentDomain_ProcessExit(object sender, EventArgs e)
-        {
-            // ToDo
-        }
-
-        /// <summary>
-        /// Handles Unhandled Exceptions and send them to AI
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="UnhandledExceptionEventArgs"/> instance containing the event data.</param>
-        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            // ToDo
-        }
     }
 }
